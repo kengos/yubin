4b7312be741228bfbf501433e6dfb112574b28a2
kengos <kengo@kengos.jp>
2012-09-09 15:34:44 +0900

    generator wip

---
+++ lib/generators/yubin/install_generator.rb
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+module Yubin
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      desc "Copy Yubin json files"
+      source_root File.expand_path('../../../../vendor/assets/javascripts/yubin', __FILE__)
+
+      def copy_initializers
+        FileUtils.cp_r source_root, 'app/assets/javascripts/'
+      end
+
+    end
+  end
+end
